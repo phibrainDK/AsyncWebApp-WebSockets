3c60a0825dd4bc4316fa3761350e72f6f734c782
phibrain <a20145549@pucp.pe>
2023-10-24 22:54:41 -0500

    nit-fix prefix local for tf files

--- terraform/core/locals.tf
+++ terraform/core/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  company             = "wds"
+  company             = "wds-ws"
   prefix              = "app-${local.company}-tf-${terraform.workspace}"
   stage               = split("-", terraform.workspace)[0]
   account_id          = data.aws_caller_identity.current.account_id
